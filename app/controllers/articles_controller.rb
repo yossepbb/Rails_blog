d2162ba33309f1cdca32b920632d2d0cf0e78920
yossepbb <b.bj@hotmail.fr>
2019-04-01 11:18:02 +0200

    CRUD DONE

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,24 +2,27 @@ class ArticlesController < ApplicationController
 
 	# CRUD = Create, Read, Update, Delete
 	
+	# Listing de tous les articles
 	def index
 		@articles = Article.all
 	end
 
-
-	def show
-		
+	#  affichage d'un article
+	def show	
 		@article = Article.find(params[:id])
 	end
 
+	# Affichage formulaire
 	def new
 		@article = Article.new
 	end
 
+
 	def edit
-		
+		@article = Article.find(params[:id])
 	end
 
+	# créer l'article et l'enrégistre dans la BD
 	def create
 		# create a new article with the attributes entered by the user
 		@article = Article.new(article_params)
@@ -33,12 +36,24 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	# Mise à jour du contenu d'un article
 	def update
-		
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
 
+	# Suppression d'un article
 	def destroy
-		
+		@article = Article.find(params[:id])
+
+		@article.destroy
+
+		redirect_to articles_path
 	end
 
 	# Methode privée
