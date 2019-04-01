dee6164d06d79a7015f46ac3264473ba16bc3005
yossepbb <b.bj@hotmail.fr>
2019-04-01 12:53:40 +0200

    add routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/index'
   root 'welcome#index'
-  resources :articles
+  
+  resources :articles do
+  	resources :comments
+  end
 
   # post 'articles/create'
 
