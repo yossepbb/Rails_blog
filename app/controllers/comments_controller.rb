e23369dd231d8a4754bf0ebd8df876c0ac737044
yossepbb <b.bj@hotmail.fr>
2019-04-01 14:56:34 +0200

    delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,10 +17,10 @@ class CommentsController < ApplicationController
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
 
-		redirect_to article_path(@article)
+		redirect_to article_path(@article)	
 		
 	end
-
+	
 	def edit
 		
 	end
@@ -30,7 +30,12 @@ class CommentsController < ApplicationController
 	end
 
 	def destroy
-		
+
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+
+		redirect_to article_path(@article)
 	end
 
 	private
