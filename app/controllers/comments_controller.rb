e7dbbeb21a70e4b63927fe176aa20076934c31a7
yossepbb <b.bj@hotmail.fr>
2019-04-01 13:31:57 +0200

    create a comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,45 @@
+class CommentsController < ApplicationController
+
+	def index
+		@comment = Comment.all
+	end
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def create
+
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.create(comment_params)
+
+		redirect_to article_path(@article)
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	private
+
+	def comment_params
+
+		params.require(:comment).permit(:commenter, :body)
+
+	end
+
+
+end
